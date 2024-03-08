ab88774439447b86285be4bed97da7663be74b9c
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-03-08 15:49:14 +0000

    teste api

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,22 @@ import {
 } from '@vercel/examples-ui'
 import { useRouter } from 'next/navigation'
 import  FormLogin  from './components/formLogin'
+import Apitest from './components/apiTest'
  
 
 
 export default function Index() {
   const router = useRouter()
 
-  async function handle(){
+/*   async function handle(){
     const result =   await fetch('/api/auth', { method: 'POST' })
     router.push('/protected')
-  }
+  } */
 
 
   return (
     <div className="p-3.5 lg:p-6 flex justify-center gap-0  lg:gap-80">   
-      <FormLogin />        
+      <Apitest />        
       <div className=""></div>        
    </div>      
   )
