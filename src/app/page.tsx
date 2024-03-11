e524a6e5d0ad904e5be54f89db5bf89496e9e354
fabianoobispo <fabianoobispo@icloud.com>
2024-03-11 13:56:51 -0300

    ativando menu 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeaderV2 from "@/components/HeaderV2";
 export default function Home() {
   return (
     <>
-    {/* <Header /> */}
+    <Header />
    {/*     <HeaderV2 /> */}
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
