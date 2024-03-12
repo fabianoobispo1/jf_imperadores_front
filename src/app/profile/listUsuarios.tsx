c0a3517413e88eac51219cb5b8add70c8ea797fc
fabianoobispo <fabianoobispo@icloud.com>
2024-03-11 23:58:52 -0300

    teste

---
+++ src/app/profile/listUsuarios.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useEffect, useState } from "react";
+import useSession from "@/lib/useSession";
+
+import { api } from "@/lib/api";
+
+export default function ListUsuarios() {
+  const sesion = useSession();
+
+  const token = sesion?.token
+
+  const [list, setList] = useState([''])
+
+  useEffect(() => {
+ 
+    if (token){  
+      atualizaLista() 
+    }  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ token]);
+
+  async function atualizaLista() {
+    try {
+      const headers: Record<string, string> = {
+       "Content-Type": "application/json",
+     };  
+     if (token) {
+       headers["Authorization"] = `Bearer ${token}`;
+     }      
+     const response = await api('/listarusuarios', {method: 'GET',headers} )
+
+     setList(await response.json())      
+     } catch (error: any) {
+       console.log(error);     
+     } 
+  }
+
+  return (
+
+
+    <div className="max-w-full w-full mx-auto overflow-hidden shadow-lg bg-ct-dark-200 rounded-2xl p-8 space-y-5 ">
+      <div className="grid grid-cols-3 gap-4">
+        <p>Nome</p>
+        <p>mail</p>
+        <p>Admin</p>
+      </div>
+
+      {list.map((iten: any) =>
+        <div key={iten.id} >
+
+          <div className="grid grid-cols-3 gap-4 border-b-2 border-fa-dourado">
+            <p>{iten.nome}</p>
+            <p>{iten.email}</p>
+            {iten.administrador? <p>Sim</p> :<p>Nao</p>  } 
+          </div>
+        </div>      
+      )}
+    </div>
+
+  );
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import { apiGetAuthUser } from "@/lib/api-requests";
 import { cookies } from "next/headers";
 import { AuthPageInvisible } from "@/lib/protect-page";
+import ListUsuarios from "./listUsuarios";
 
 export default async function ProfilePage() {
   const cookieStore = cookies();
@@ -15,15 +16,12 @@ export default async function ProfilePage() {
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
           <div>
             <p className="mb-3 text-5xl text-center font-semibold">
-              Pagina logada 
+              Bem vindo, {user.name}
             </p>
-            <div className="mt-8">
+            <div className="mt-8 m-2">
              {/*  <p className="mb-3">Id: {user.id}</p> */}
-              <p className="mb-3">Name: {user.name}</p>
-              <p className="mb-3">Email: {user.email}</p>
-   {/*            <p className="mb-3">Role: {user.role}</p> */}
-              <p className="mb-3">Verificado: {String(user.verified)}</p>
-              <p className="mb-3">Verified: {String(user.administrador)}</p>
+              <p className="mb-3">Usuarios registrados:</p>
+                <ListUsuarios />
             </div>
           </div>
         </div>
