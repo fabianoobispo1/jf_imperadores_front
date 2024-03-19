f8953d88c5f7289b28b6496fe4d68001c2e1a121
fabianoobispo <fabianoobispo@icloud.com>
2024-03-18 21:20:40 -0300

    tela de cadastro de transacoes

--- src/app/transacoes/transacao-form-list.tsx
+++ src/app/transacoes/transacao-form-list.tsx
@@ -3,7 +3,7 @@
 import { FaTransacoesInput, FaTransacoesSchema } from "@/lib/validations/user.schema";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useLayoutEffect } from "react";
 import FormInput from "@/components/FormInput";
 import { LoadingButton } from "@/components/LoadingButton";
 import useStore from "@/store";
@@ -27,6 +27,9 @@ export default function TransacaoFormList() {
 
   const [list, setList] = useState([''])
   const [carregandoLista, setCarregandoLista] = useState(true)
+  const [totalPagamentos, setTotalPagamentos] = useState(0)
+  const [totalRecebimentos, setTotalRecebimentos] = useState(0)
+
 
   const methods = useForm<FaTransacoesInput>({
     resolver: zodResolver(FaTransacoesSchema),
@@ -43,10 +46,15 @@ export default function TransacaoFormList() {
       reset();
     }
     if (token){  
-      atualizaLista() 
-    }  
+      atualizaLista()   
+     } 
+     
+     if (carregandoLista == false){
+      contador(list)
+     } 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSubmitSuccessful, token]);
+  }, [isSubmitSuccessful, token, carregandoLista]);
+
 
 
   async function addFaTransacaoFunction(faTransacao: FaTransacoesInput) {    
@@ -99,7 +107,9 @@ export default function TransacaoFormList() {
   };
 
   async function atualizaLista() {
-    setCarregandoLista(true)    
+    setCarregandoLista(true)  
+    setTotalPagamentos(0)
+    setTotalRecebimentos(0)  
     try {
       const headers: Record<string, string> = {
        "Content-Type": "application/json",
@@ -110,12 +120,50 @@ export default function TransacaoFormList() {
      const response = await api('/fatransacaolistar', {method: 'GET',headers} )
 
      setList(await response.json())  
+
+/*      list.map((iten: any) =>{
+      if(iten.tipo =='P'){
+        if(iten.valor){
+          setTotalPagamentos(totalPagamentos+iten.valor) 
+        }     
+      }else{
+        if(iten.valor){
+          setTotalRecebimentos(totalRecebimentos+iten.valor)
+        }
+      }
+     })
+ */
+/*      console.log(totalPagamentos)
+     console.log(totalRecebimentos)
+ */
      setCarregandoLista(false)    
      } catch (error: any) {
        console.log(error);     
      } 
-  }
 
+  
+  }
+  async function contador(list:any ) {
+    let totalRecebimento: any	 
+    let totalPagamento: any	 
+    totalRecebimento = 0
+    totalPagamento = 0
+    list.map((iten: any) =>{
+
+      if(iten.tipo =='P'){
+        if(iten.valor){
+          totalPagamento = totalPagamento+iten.valor
+        }     
+      }else{
+        if(iten.valor){
+          totalRecebimento = totalRecebimento + iten.valor
+        }
+      }
+     })
+     setTotalPagamentos(totalPagamento)
+     setTotalRecebimentos(totalRecebimento)
+  
+  }
   
   async function deleteRow(id:string) {
     try {
@@ -148,7 +196,7 @@ export default function TransacaoFormList() {
       >
 
       <div className="grid grid-cols-1 gap-4">
-        <FormInput label="Titulo" name="titulo" type="text" />
+        <FormInput label="Título" name="titulo" type="text" />
       </div>
 
       <div className="grid grid-cols-3 gap-4">
@@ -175,59 +223,61 @@ export default function TransacaoFormList() {
     <div className="max-w-full w-full mx-auto overflow-hidden  bg-transparent rounded-md p-8 space-y-5"></div>
 
     <div className="max-w-full w-full mx-auto overflow-hidden shadow-lg bg-ct-dark-200 rounded-md p-8 space-y-5">
-      <div className="grid grid-cols-5 gap-4">
-        <p>Titulo</p>
+      <div className="grid grid-cols-2 gap-4 justify-between">
+        <p>Título</p>
+        <p className="text-right">Opções</p>
+      </div>
+      <div className="grid grid-cols-3 gap-4 ">
+           
         <p>Valor</p>
-        <p>Tipo</p>
-        <p>Venciemnto</p>
-        <p>Opcoes</p>
+        <p  className="text-center">Tipo</p>
+        <p  className="text-right">Vencimento</p>
+
       </div>
     
-      {carregandoLista == true?
+      {carregandoLista == true? 
       <p>carregando</p>
       :
       <>
-            {list.map((iten: any) =>
+        {list.map((iten: any) =>
+        
         <div key={iten.id} >
-
-          <div className="grid grid-cols-2 gap-4">
+       
+          <div className="grid grid-cols-2 gap-4 justify-between">
             <p>{iten.titulo}</p>
-            <div>
+            <div className="grid justify-items-end">
               <button onClick={() => {deleteRow(iten.id)}} >
                 <Trash  />
               </button>          
             </div>  
            </div>
            <div className="grid grid-cols-3 gap-4 border-b-2 border-fa-dourado">
-           <p>{iten.valor}</p>
-            {iten.tipo =='P'? <p>Pagamento</p> :<p>Recebiemnto</p>  }
+            <p>{iten.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+            {iten.tipo =='P'?             
+            <p className="text-center">Pagamento</p>             
+            :
+            <p className="text-center">Recebimento</p>  }
 
-            <p>{format(new Date(iten.vencimento), "dd/MM/yyyy")}</p>
+            
+
+            
+            <p  className="text-right">{format(new Date(iten.vencimento), "dd/MM/yyyy")}</p>
           
            </div>
-
-         {/*  <div className="grid grid-cols-5 gap-4 border-b-2 border-fa-dourado">
-            <p>{iten.titulo}</p>
-            <p>{iten.valor}</p>
-            {iten.tipo =='P'? <p>Pagamento</p> :<p>Recebiemnto</p>  }
-
-            <p>{format(new Date(iten.vencimento), "dd/MM/yyyy")}</p>
-            <div>
-              <button onClick={() => {deleteRow(iten.id)}} >
-                <Trash  />
-              </button>          
-            </div>  
-
-          </div> */}
         </div>      
       )}
       </>}
 
    
         <div className="grid ">
-          Total
+          <p>Total Pagamentos:</p>
+          <p>{totalPagamentos.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
         </div>
 
+        <div className="grid ">
+          <p>Total Recebiemntos:</p>
+          <p>{totalRecebimentos.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+        </div>
 
     </div>
 
@@ -235,3 +285,5 @@ export default function TransacaoFormList() {
 
   );
 }
+
+
