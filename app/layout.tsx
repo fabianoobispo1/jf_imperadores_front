983024789f188baf79e180130c5a80db56ccef33
Your Name <fabianoobispo@icloud.com>
2024-06-28 17:04:22 -0300

    teste

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={'font-inter overflow-hidden'}>
         <NextTopLoader showSpinner={false} />
         <Providers session={session}>
