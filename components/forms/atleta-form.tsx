01055197f438da4fd94e0f483a0875529f250c87
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-07-09 14:20:16 +0000

    teste

--- components/forms/atleta-form.tsx
+++ components/forms/atleta-form.tsx
@@ -2,15 +2,15 @@
 import * as z from 'zod';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Trash } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
+import InputMask from 'react-input-mask';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -25,26 +25,38 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select';
-import { Checkbox } from '@/components/ui/checkbox';
-// import FileUpload from "@/components/FileUpload";
 import { useToast } from '../ui/use-toast';
-import FileUpload from '../file-upload';
 import { AlertModal } from '../modal/alert-modal';
-
-
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { format } from 'date-fns';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { Calendar } from '../ui/calendar';
+import { ptBR } from 'date-fns/locale';
+import { cn } from '@/lib/utils';
 
 const formSchema = z.object({
+  cpf: z
+    .string()
+    .min(14, 'CPF inválido')
+    .max(14, 'CPF inválido'),
   nome: z
     .string()
     .min(3, { message: 'Nome é obrigatorio' }),
+  email: z
+    .string()
+    .email({ message: 'email é obrigatorio' }),
+  data_nascimento: z
+    .date({ required_error: 'Data de nascimento é obrigatorio' }),
+  data_inicio: z
+    .date({ required_error: 'Data de inicio é obrigatorio' }),
   setor: z
-    .string()
-    .min(3, { message: 'Setor é obrigatorio' }),
+    .string(),
   posicao: z
-    .string()
-    .min(3, { message: 'Posição é obrigatorio' }),
+    .string(),
   numero: z.coerce.number(),
-  ativo: z.enum(['true', 'false']),
+  altura: z.coerce.number(),
+  peso: z.coerce.number(),
+  ativo: z.enum(['true', 'false']), 
 });
 
 type AtletaFormValues = z.infer<typeof formSchema>;
@@ -53,21 +65,33 @@ interface AtletaFormProps {
   id: string;
 }
 
-export const AtletaForm: React.FC<AtletaFormProps> = ({
-  id
-}) => {
-
+export const AtletaForm: React.FC<AtletaFormProps> = ({ id }) => {
   const router = useRouter();
   const { toast } = useToast();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-  const title = id == 'create' ? 'Novo atleta' : 'Editar Atleta';
-  const description = id =='create' ?  'Adicionar informações de um novo atleta' : 'Editar informações de um atleta';
-  const action = id  == 'create' ? 'Salvar' : 'Salvar alterações';
+  const title = id === 'create' ? 'Novo atleta' : 'Editar Atleta';
+  const description = id === 'create' ? 'Adicionar informações de um novo atleta' : 'Editar informações de um atleta';
+  const action = id === 'create' ? 'Salvar' : 'Salvar alterações';
 
   const form = useForm<AtletaFormValues>({
-    resolver: zodResolver(formSchema)
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cpf: '',
+      nome: '',
+      email: '',
+      data_nascimento: undefined,
+      data_inicio: undefined,
+      setor: '',
+      posicao: '',
+      numero: undefined,
+      altura: undefined,
+      peso: undefined,
+      ativo: 'true',
+    },
   });
+
+ 
 
   const onSubmit = async (data: AtletaFormValues) => {
     const finalData = {
@@ -78,26 +102,22 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
 
     try {
       setLoading(true);
-      if (id == 'create') {
+      if (id === 'create') {
         // await axios.post(`/api/products/edit-product/${id._id}`, data);
-    
-    
-        toast({         
+        toast({
           title: 'OK',
           description: 'Atleta Salvo'
         });
-      
       } else {
-        toast({         
+        toast({
           title: 'OK',
           description: 'Atleta alterado'
         });
         // const res = await axios.post(`/api/products/create-product`, data);
         // console.log("product", res);
       }
-   /*    router.refresh();
+      /* router.refresh();
       router.push(`/dashboard/atleta`); */
-
     } catch (error: any) {
       toast({
         variant: 'destructive',
@@ -112,7 +132,7 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true);
-      //   await axios.delete(`/api/${params.storeId}/products/${params.productId}`);
+      // await axios.delete(`/api/${params.storeId}/products/${params.productId}`);
       router.refresh();
       router.push(`/dashboard/atleta`);
     } catch (error: any) {
@@ -120,7 +140,7 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
       setLoading(false);
       setOpen(false);
     }
-  }
+  };
 
   return (
     <>
@@ -132,8 +152,8 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
       />
       <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
-        {id != 'create' && (
-          <Button 
+        {id !== 'create' && (
+          <Button
             disabled={loading}
             variant="destructive"
             size="sm"
@@ -149,8 +169,33 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="w-full space-y-8"
         >
-
           <div className="gap-8 md:grid md:grid-cols-3">
+            <Controller
+              control={form.control}
+              name="cpf"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>CPF</FormLabel>
+                  <FormControl>
+                    <InputMask
+                      mask="999.999.999-99"
+                      value={field.value}
+                      onChange={field.onChange}
+                      onBlur={field.onBlur}
+                    >
+
+                      {(inputProps) => (
+                        <Input
+                          {...inputProps}
+                          placeholder="cpf"
+                        />
+                      )}
+                    </InputMask>
+                  </FormControl>
+                  <FormMessage>{form.formState.errors.cpf?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="nome"
@@ -170,14 +215,14 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
             />
             <FormField
               control={form.control}
-              name="setor"
+              name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Setor</FormLabel>
+                  <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
                       disabled={loading}
-                      placeholder="Ataque, Defesa ... "
+                      placeholder="email"
                       {...field}
                     />
                   </FormControl>
@@ -187,34 +232,91 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
             />
             <FormField
               control={form.control}
-              name="posicao"
+              name="data_nascimento"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Posição</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="QB, OL, LT ...."
-                      {...field}
-                    />
-                  </FormControl>
+                  <FormLabel>Data de Nascimento</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full pl-3 text-left font-normal',
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          {field.value ? (
+                            format(new Date(field.value), 'dd/MM/yyyy', {
+                              locale: ptBR
+                            })
+                          ) : (
+                            <span>Escolha uma data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        fromYear={1960}
+                        toYear={2024}
+                        captionLayout="dropdown-buttons"
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
             />
             <FormField
               control={form.control}
-              name="numero"
+              name="data_inicio"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Numero</FormLabel>
-                  <FormControl>
-                    <Input type="number" disabled={loading} {...field} />
-                  </FormControl>
+                  <FormLabel>Data de inicio</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full pl-3 text-left font-normal',
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          {field.value ? (
+                            format(new Date(field.value), 'dd/MM/yyyy', {
+                              locale: ptBR
+                            })
+                          ) : (
+                            <span>Escolha uma data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        fromYear={1960}
+                        toYear={2024}
+                        captionLayout="dropdown-buttons"
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
             <FormField
               control={form.control}
               name="ativo"
@@ -223,7 +325,7 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
                   <FormLabel>Atleta Ativo</FormLabel>
                   <Select
                     disabled={loading}
-                    onValueChange={field.onChange}
+                    onValueChange={(value) => field.onChange(value)}
                     value={field.value}
                     defaultValue={field.value}
                   >
@@ -231,21 +333,21 @@ export const AtletaForm: React.FC<AtletaFormProps> = ({
                       <SelectTrigger>
                         <SelectValue
                           defaultValue={field.value}
-                          placeholder="..."
+                          placeholder="?"
                         />
                       </SelectTrigger>
                     </FormControl>
                     <SelectContent>
-                <SelectItem value="true">Sim</SelectItem>
-                <SelectItem value="false">Não</SelectItem>
-              </SelectContent>
+                      <SelectItem value="true">Sim</SelectItem>
+                      <SelectItem value="false">Não</SelectItem>
+                    </SelectContent>
                   </Select>
                   <FormMessage />
                 </FormItem>
               )}
             />
           </div>
-          <Button disabled={loading} className="ml-auto" type="submit">
+          <Button type="submit" disabled={loading}>
             {action}
           </Button>
         </form>
