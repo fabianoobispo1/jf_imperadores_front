df55bc82c48f5fbc316f3e464a014e1b24c98ea4
fabianoobispo1 <fabianoobispo@icloud.com>
2024-09-07 15:05:04 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ export const metadata: Metadata = {
   title: 'JF Imperadores',
   description: 'Pagina inicial JF Imperadores'
 };
+
 export default function HomePage() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
-        <Image 
-        alt='Campo-de-futebol-americano.jpg'
-         src={'/Campo-de-futebol-americano.jpg'}
-         fill={true}
-         objectFit='cover'
-        
-      />
-         
+        <Image
+          alt="Campo-de-futebol-americano.jpg"
+          src={'/Campo-de-futebol-americano.jpg'}
+          fill={true}
+          objectFit="cover"
+        />
+
         <div className="relative z-20 flex items-center text-lg font-bold text-primary">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ export default function HomePage() {
               me ajudou a entregar designs impressionantes para meus clientes mais rápido do que nunca.&rdquo; */}
               {/* &ldquo;Um texto....&rdquo; */}
             </p>
-           {/*  <footer className="text-sm text-black">By Bispo</footer> */}
+            {/*  <footer className="text-sm text-black">By Bispo</footer> */}
           </blockquote>
         </div>
       </div>
