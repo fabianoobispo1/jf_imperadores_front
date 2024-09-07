65ecba15aa609be039c5fa201b0aa330188c9811
fabianoobispo1 <fabianoobispo@icloud.com>
2024-09-07 17:00:59 -0300

    tste

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,8 @@ export default function HomePage() {
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
   {/*         JF Imperadores */}
+
+  
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
