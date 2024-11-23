f982ce0f49663399c74f42f5f5e3a741289559fe
fabianoobispo1 <fabianoobispo@icloud.com>
2024-11-22 21:22:23 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+      {/*  <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <Image
           alt="Campo-de-futebol-americano.jpg"
@@ -34,20 +34,14 @@ export default function HomePage() {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          {/*         JF Imperadores */}
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg">
-              {/* &ldquo;Esta biblioteca me poupou incontáveis horas de trabalho e
-              me ajudou a entregar designs impressionantes para meus clientes mais rápido do que nunca.&rdquo; */}
-              {/* &ldquo;Um texto....&rdquo; */}
-            </p>
-            {/*  <footer className="text-sm text-black">By Bispo</footer> */}
+            <p className="text-lg"></p>
           </blockquote>
         </div>
       </div>
-      <AuthenticationModal />
+      <AuthenticationModal /> */}
     </div>
   );
 }
