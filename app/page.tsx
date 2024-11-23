895d7e7f27d8eefcf7829b01a33dcc15562baeb7
fabianoobispo1 <fabianoobispo@icloud.com>
2024-11-23 20:18:28 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import AuthenticationModal from '@/components/signin/authentication-modal';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Redirecionador from '@/components/redirecionador';
 
 export const metadata: Metadata = {
   title: 'JF Imperadores',
@@ -11,7 +13,8 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="flex h-screen items-center justify-center ">
+      <Redirecionador link={'https://jfimperadores.lojavirtualnuvem.com.br'} />
       {/*  <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <Image
