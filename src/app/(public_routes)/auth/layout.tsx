c9f65cd30435413f078e8b08fc1f04c573c833e1
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-11-25 19:38:44 +0000

    teste

--- src/app/(public_routes)/auth/layout.tsx
+++ src/app/(public_routes)/auth/layout.tsx
@@ -9,7 +9,7 @@ export default async function PublicLayout({
 }) {
   const session = await auth()
 
-  if (session?.user) redirect('/home')
+  if (session?.user) redirect('/admin')
 
   return <div>{children}</div>
 }
