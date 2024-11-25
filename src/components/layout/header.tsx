6c4b8076e7404d9922bb44dbf87e3ae2a3887d7d
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-11-25 20:31:53 +0000

    test

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,16 +5,13 @@ import { cn } from '@/lib/utils'
 
 import { MobileSidebar } from './mobile-sidebar'
 import { UserNav } from './user-nav'
-
+// so mudar para botton-0
 export default function Header() {
   return (
     <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
       <nav className="flex h-14 items-center justify-between px-4">
         <div className={cn('hidden md:!block')}>
-          <Link
-            href={'/dashboard'}
-            /* target="_blank" */
-          >
+          <Link href={'/dashboard'}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
