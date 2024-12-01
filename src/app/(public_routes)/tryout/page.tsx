372e8d2874f97f01258d1e87d5d73c438ebe37b7
fabianoobispo1 <fabianoobispo@icloud.com>
2024-12-01 12:32:51 -0300

    teste

---
+++ src/app/(public_routes)/tryout/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'JF Imperadores - Seltiva',
+  description: 'Pagina inscrição Seletiva',
+  keywords: 'JF Imperadores , Seltiva futebol amricano, fabr, inscrição',
+}
+
+export default function auth2() {
+  return (
+    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      Seltiva
+    </div>
+  )
+}
