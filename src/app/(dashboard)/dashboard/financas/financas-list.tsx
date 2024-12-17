49fedcda93bbdd58f9d9b6619ff4de183423d6b4
Your Name <fabianoobispo@icloud.com>
2024-12-17 16:37:37 -0300

    teste

--- src/app/(dashboard)/dashboard/financas/financas-list.tsx
+++ src/app/(dashboard)/dashboard/financas/financas-list.tsx
@@ -19,59 +19,87 @@ import { Button } from '@/components/ui/button'
 import type { Id } from '../../../../../convex/_generated/dataModel'
 import { api } from '../../../../../convex/_generated/api'
 
-interface Seletivas {
-  _id: Id<'seletiva'>
-  _creationTime: number
-  nome: string
-  cpf: string
-  email: string
-  data_nascimento?: number
-}
-/* interface Transacoes {
+interface Transacoes {
   _id: Id<'transacao'>
   _creationTime: number
   tipo: string
   descricao: string
   valor: number
   data: number
-} */
-export function TryoutList() {
+}
+
+export function FinancasList() {
   const [loading, setLoading] = useState<boolean>(false)
+  const [transacoes, setTransacoes] = useState<Transacoes[]>([])
+  const [currentMonth, setCurrentMonth] = useState<number>(
+    new Date().getMonth() + 1, // Mês atual (1 = Janeiro)
+  )
+  const [currentYear, setCurrentYear] = useState<number>(
+    new Date().getFullYear(),
+  )
 
-  const [seletivas, setSeletivas] = useState<Seletivas[]>([])
-  /*  const [transacoes, setTransacoes] = useState<Transacoes[]>([]) */
+  const getMonthTimestamps = (year: number, month: number) => {
+    // O mês em JavaScript é baseado em zero (0 = Janeiro, 11 = Dezembro)
+    const firstDay = new Date(year, month - 1, 1) // Primeiro dia do mês
+    const lastDay = new Date(year, month, 0) // Último dia do mês
 
-  const [offset, setOffset] = useState(0)
-  const limit = 10
+    const timestampStart = firstDay.getTime() // Início do mês em timestamp
+    const timestampEnd = lastDay.setHours(23, 59, 59, 999) // Final do mês em timestamp
 
-  const fetchSeletivaPaginated = async (offset: number, limit: number) => {
+    return { timestampStart, timestampEnd }
+  }
+
+  const fetchSeletivaByMonth = async (year: number, month: number) => {
     setLoading(true)
+    // Obtém os timestamps do mês
+    const { timestampStart, timestampEnd } = getMonthTimestamps(year, month)
+    console.log('Timestamp Inicial:', timestampStart)
+    console.log('Timestamp Final:', timestampEnd)
     try {
-      const seletivas = await fetchQuery(api.seletiva.getAllPaginated, {
-        limit,
-        offset,
+      // Buscar dados para o mês e ano selecionado
+      const transacoes = await fetchQuery(api.transacao.buscaPeriodo, {
+        dataInicial: timestampStart,
+        dataFinal: timestampEnd,
       })
-      setSeletivas(seletivas)
+      setTransacoes(transacoes)
     } catch (error) {
-      console.error('Erro ao buscar seletivas paginadas:', error)
+      console.error('Erro ao buscar seletivas:', error)
     } finally {
       setLoading(false)
     }
   }
 
   useEffect(() => {
-    fetchSeletivaPaginated(offset, limit)
-  }, [offset])
+    fetchSeletivaByMonth(currentMonth, currentYear)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentMonth])
 
-  const handleNext = () => setOffset((prev) => prev + limit)
-  const handlePrev = () => setOffset((prev) => Math.max(prev - limit, 0))
+  const handleNextMonth = () => {
+    setCurrentMonth((prevMonth) => {
+      if (prevMonth === 12) {
+        setCurrentYear((prevYear) => prevYear + 1)
+        return 1 // Volta para Janeiro
+      }
+      return prevMonth + 1
+    })
+  }
 
-  const removeCandidato = async (id: Id<'seletiva'>) => {
+  const handlePrevMonth = () => {
+    setCurrentMonth((prevMonth) => {
+      if (prevMonth === 1) {
+        setCurrentYear((prevYear) => prevYear - 1)
+        return 12 // Volta para Dezembro
+      }
+      return prevMonth - 1
+    })
+  }
+
+  /*   const removeCandidato = async (id: Id<'seletiva'>) => {
     setLoading(true)
     await fetchMutation(api.seletiva.remove, { seletivaId: id })
     fetchSeletivaPaginated(offset, limit)
     setLoading(false)
-  }
+  } */
 
   return (
     <div className="space-y-8">
@@ -94,24 +122,24 @@ export function TryoutList() {
                 </TableCell>
               </TableRow>
             ) : (
-              seletivas.map((seletiva) => (
-                <TableRow key={seletiva._id}>
-                  <TableCell>{seletiva.nome}</TableCell>
+              transacoes.map((transacoe) => (
+                <TableRow key={transacoe._id}>
+                  {/*  <TableCell>{seletiva.nome}</TableCell>
                   <TableCell>{seletiva.email}</TableCell>
                   <TableCell className="text-center">
                     {seletiva.data_nascimento
                       ? new Date(seletiva.data_nascimento).toLocaleDateString()
                       : '-'}
                   </TableCell>
-                  <TableCell className="flex items-center justify-center gap-2">
-                    <LoadingButton
+                  <TableCell className="flex items-center justify-center gap-2"> */}
+                  {/*   <LoadingButton
                       loading={loading}
                       variant={'destructive'}
                       onClick={() => removeCandidato(seletiva._id)}
                     >
                       <Trash className="h-4 w-4" />
-                    </LoadingButton>
-                  </TableCell>
+                    </LoadingButton> */}
+                  {/* </TableCell> */}
                 </TableRow>
               ))
             )}
@@ -120,32 +148,28 @@ export function TryoutList() {
 
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
+      <div className="flex flex-row items-center justify-center space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={handlePrevMonth}
+          disabled={loading}
+        >
+          Anterior
+        </Button>
+        <div className="flex-1 text-sm text-muted-foreground text-center">
           <p>
-            Página {Math.ceil(offset / limit) + 1} de{' '}
-            {Math.ceil(seletivas.length / limit)}
+            Mês: {String(currentMonth).padStart(2, '0')}/{currentYear}
           </p>
         </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={handlePrev}
-            disabled={offset === 0 || loading}
-          >
-            Anterior
-          </Button>
-
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={handleNext}
-            disabled={loading || seletivas.length < limit}
-          >
-            Próxima
-          </Button>
-        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={handleNextMonth}
+          disabled={loading}
+        >
+          Próxima
+        </Button>
       </div>
     </div>
   )
