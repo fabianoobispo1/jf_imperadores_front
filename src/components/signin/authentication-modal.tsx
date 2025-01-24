85a81fa8b87b19565c633b919f6d1f511636c807
fabianoobispo1 <fabianoobispo@icloud.com>
2025-01-24 10:30:48 -0300

    teste

--- src/components/signin/authentication-modal.tsx
+++ src/components/signin/authentication-modal.tsx
@@ -104,7 +104,7 @@ export default function AuthenticationModal() {
       <div className="flex h-full items-center p-4 lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col justify-center items-center space-y-2 text-center">
-            <h1 className="text-3xl font-bold tracking-tight pb-8">
+            <h1 className="text-3xl font-bold tracking-tight pb-8 text-red-500">
               Homologação
             </h1>
             <Image src={'/logo.png'} alt="logo" width={250} height={250} />
