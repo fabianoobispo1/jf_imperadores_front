25e7ca44b443edd9cd6ff73f759d522f375f8d23
fabianoobispo1 <fabianoobispo@icloud.com>
2025-01-24 10:42:47 -0300

    testes

--- src/app/(public_routes)/entrar/page.tsx
+++ src/app/(public_routes)/entrar/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
 
 import AuthenticationModal from '@/components/signin/authentication-modal'
 
@@ -12,20 +13,26 @@ export const metadata: Metadata = {
 export default function auth2() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      {/*  <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
-        
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+        <Image
+          src="/Campo-de-futebol-americano.jpg"
+          alt="Campo de futebol americano"
+          fill
+          className="object-cover opacity-80" // opacity helps text remain readable
+          priority
+        />
+
         <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2"> */}
-      <p className="text-lg">
-        {/* &ldquo;Esta biblioteca me poupou incontáveis horas de trabalho e
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              {/* &ldquo;Esta biblioteca me poupou incontáveis horas de trabalho e
               me ajudou a entregar designs impressionantes para meus clientes mais rápido do que nunca.&rdquo; */}
-        {/* &ldquo;Um texto....&rdquo; */}
-      </p>
-      {/*    </blockquote>
+              {/* &ldquo;Um texto....&rdquo; */}
+            </p>
+          </blockquote>
         </div>
       </div>
- */}
+
       {/*     <ResetPasswordEmail idReset="12" nome="fabiano" /> */}
       <AuthenticationModal />
     </div>
