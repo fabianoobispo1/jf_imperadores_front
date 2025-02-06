3612f20269a47dd5b9862e1a2d8d391a10524e64
fabianoobispo1 <fabianoobispo@icloud.com>
2025-02-06 19:44:32 -0300

    teste

--- src/components/DadosIniciais.tsx
+++ src/components/DadosIniciais.tsx
@@ -54,133 +54,139 @@ export default function DadosIniciais() {
   }
 
   return (
-    <div className="grid gap-4 md:grid-cols-3">
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Pix para mensalidade do mês
-          </CardTitle>
-          <QrCodeIcon className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <p>cnpj: 27.871.530/0001-61</p>
-          <Image
-            src="/qr-code.png"
-            alt="QR Code para pagamento"
-            width={200}
-            height={200}
-          />
-          <span className="text-sm text-muted-foreground">
-            Ao realizar o pagamento envie o comprovante para o MH no whatsapp
-          </span>
-        </CardContent>
-      </Card>
+    <>
+      <div className="grid gap-4 md:grid-cols-3">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Pix para mensalidade do mês
+            </CardTitle>
+            <QrCodeIcon className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <p>cnpj: 27.871.530/0001-61</p>
+            <Image
+              src="/qr-code.png"
+              alt="QR Code para pagamento"
+              width={200}
+              height={200}
+            />
+            <span className="text-sm text-muted-foreground">
+              Ao realizar o pagamento envie o comprovante para o MH no whatsapp
+            </span>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Total de Atletas
+            </CardTitle>
+            <UsersIcon className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {totalAtletas === undefined ? (
+                <Skeleton className="h-8 w-20" />
+              ) : (
+                totalAtletas || 0
+              )}
+            </div>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Total de Atletas
-          </CardTitle>
-          <UsersIcon className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            {totalAtletas === undefined ? (
+        <Card
+          className={
+            session?.user?.role === 'admin'
+              ? 'cursor-pointer hover:bg-gray-100'
+              : ''
+          }
+          onClick={handleAtletaClick}
+        >
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Atletas Ativos + Aprovados
+            </CardTitle>
+            <UserCheckIcon className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {totalAtletasAtivos === undefined ? (
+                <Skeleton className="h-8 w-20" />
+              ) : (
+                totalAtletasAtivos || 0
+              )}
+              {' + '}
+              {totalAprovados === undefined ? (
+                <Skeleton className="h-8 w-20" />
+              ) : (
+                totalAprovados || 0
+              )}
+            </div>
+            {' Total '}
+            {totalAtletasAtivos === undefined ||
+            totalAprovados === undefined ? (
               <Skeleton className="h-8 w-20" />
             ) : (
-              totalAtletas || 0
+              (totalAtletasAtivos || 0) + (totalAprovados || 0)
             )}
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
 
-      <Card
-        className={
-          session?.user?.role === 'admin'
-            ? 'cursor-pointer hover:bg-gray-100'
-            : ''
-        }
-        onClick={handleAtletaClick}
-      >
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Atletas Ativos + Aprovados
-          </CardTitle>
-          <UserCheckIcon className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            {totalAtletasAtivos === undefined ? (
-              <Skeleton className="h-8 w-20" />
-            ) : (
-              totalAtletasAtivos || 0
-            )}
-            {' + '}
-            {totalAprovados === undefined ? (
-              <Skeleton className="h-8 w-20" />
-            ) : (
-              totalAprovados || 0
-            )}
-          </div>
-          {' Total '}
-          {totalAtletasAtivos === undefined || totalAprovados === undefined ? (
-            <Skeleton className="h-8 w-20" />
-          ) : (
-            (totalAtletasAtivos || 0) + (totalAprovados || 0)
-          )}
-        </CardContent>
-      </Card>
+        <Card
+          className={
+            session?.user?.role === 'admin'
+              ? 'cursor-pointer hover:bg-gray-100'
+              : ''
+          }
+          onClick={handleSeletivaClick}
+        >
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Inscrições para a Seletiva
+            </CardTitle>
+            <UserPlusIcon className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {totalSeletiva === undefined ? (
+                <Skeleton className="h-8 w-20" />
+              ) : (
+                totalSeletiva || 0
+              )}
+            </div>
+          </CardContent>
+        </Card>
 
-      <Card
-        className={
-          session?.user?.role === 'admin'
-            ? 'cursor-pointer hover:bg-gray-100'
-            : ''
-        }
-        onClick={handleSeletivaClick}
-      >
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Inscrições para a Seletiva
-          </CardTitle>
-          <UserPlusIcon className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            {totalSeletiva === undefined ? (
-              <Skeleton className="h-8 w-20" />
-            ) : (
-              totalSeletiva || 0
-            )}
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card className="cursor-pointer hover:bg-gray-100">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Últimos Treinos</CardTitle>
-          <CalendarCheck2 className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-2">
-            {presencasPorData === undefined ? (
-              <Skeleton className="h-20 w-full" />
-            ) : (
-              Object.entries(presencasPorData)
-                .sort(([dataA], [dataB]) => Number(dataB) - Number(dataA))
-                .slice(0, 3)
-                .map(([data, total]) => (
-                  <div key={data} className="flex justify-between text-sm">
-                    <span>
-                      {format(Number(data), 'dd/MM/yyyy', { locale: ptBR })}
-                    </span>
-                    <span>{total} atletas</span>
-                  </div>
-                ))
-            )}
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+        <Card className="cursor-pointer hover:bg-gray-100">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Últimos Treinos
+            </CardTitle>
+            <CalendarCheck2 className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2">
+              {presencasPorData === undefined ? (
+                <Skeleton className="h-20 w-full" />
+              ) : (
+                Object.entries(presencasPorData)
+                  .sort(([dataA], [dataB]) => Number(dataB) - Number(dataA))
+                  .slice(0, 3)
+                  .map(([data, total]) => (
+                    <div key={data} className="flex justify-between text-sm">
+                      <span>
+                        {format(Number(data), 'dd/MM/yyyy', { locale: ptBR })}
+                      </span>
+                      <span>{total} atletas</span>
+                    </div>
+                  ))
+              )}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
