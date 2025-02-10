4d1fd85332a98db228e44c9174aa9192acf9159f
fabianoobispo1 <fabianoobispo@icloud.com>
2025-02-10 11:02:39 -0300

    teste

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -136,22 +136,28 @@ const authConfig = {
       return true
     },
     async jwt({ token, user }) {
+      console.log('JWT Callback - Token antes:', token)
+      console.log('JWT Callback - User:', user)
       if (user) {
         token.id = user.id as string
         token.role = user.role
         token.image = user.image as string
         token.nome = user.nome as string
       }
+      console.log('JWT Callback - Token depois:', token)
       return token
     },
 
     async session({ session, token }) {
+      console.log('Session Callback - Token:', token)
+      console.log('Session Callback - Session antes:', session)
       if (token.role) {
         session.user.id = token.id
         session.user.role = token.role
         session.user.image = token.image
         session.user.nome = token.nome
       }
+      console.log('Session Callback - Session depois:', session)
       return session
     },
   },
